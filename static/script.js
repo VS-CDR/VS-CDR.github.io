9830334411f6da9157103fc034a967d2912267da
Vlad Shubin <shubin.va@phystech.edu>
2023-05-14 21:33:03 +0300

    добавил темную тему

---
+++ static/script.js
@@ -0,0 +1,31 @@
+const theme_btn = document.querySelector(".header__theme-button");
+
+function ChangeTheme() {
+    let elem = document.querySelector(".header__theme-button")
+    elem.classList.toggle("header__theme-button_dark")
+    elem.classList.toggle("header__theme-button_light")
+
+    elem = document.querySelector(".header");
+    elem.classList.toggle('header_dark');
+    elem.classList.toggle('header_light');
+
+    elem = document.querySelector(".header__text");
+    elem.classList.toggle('header__text_dark');
+    elem.classList.toggle('header__text_light');
+
+    elem = document.querySelector(".info");
+    elem.classList.toggle('info_dark');
+    elem.classList.toggle('info_light');
+
+    elem = document.querySelector(".footer");
+    elem.classList.toggle('footer_dark');
+    elem.classList.toggle('footer_light');
+
+    elem = document.querySelector(".footer__text");
+    elem.classList.toggle('footer__text_dark');
+    elem.classList.toggle('footer__text_light');
+}
+
+theme_btn.addEventListener("click", ChangeTheme);
+
+const elementsWithTheme = document.querySelectorAll(".theme");
