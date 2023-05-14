90ad4cc18506c75a7313d7a89cdb5f39e6720227
Vlad Shubin <shubin.va@phystech.edu>
2023-05-14 23:15:10 +0300

    добавил дождь

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 const theme_btn = document.querySelector(".header__theme-button");
 
 function ChangeTheme() {
-    let elem = document.querySelector(".header__theme-button")
+    let elem = document.querySelector(".header__theme-button");
     elem.classList.toggle("header__theme-button_dark")
     elem.classList.toggle("header__theme-button_light")
 
@@ -32,5 +32,27 @@ function ChangeTheme() {
 
 theme_btn.addEventListener("click", ChangeTheme);
 
+
+
+const rain_btn = document.querySelector(".footer__rain-button");
 let rain = document.querySelector(".rain");
 
+rain_btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (window.innerWidth >= 800) {
+        rain.classList.add("rain_active");
+        rain.classList.add("fall_animation");
+    }
+})
+
+window.addEventListener('resize', function () {
+    if (rain.classList.contains("rain_active")) {
+        rain.classList.toggle("rain_animation");
+    }
+})
+
+rain.addEventListener('click', function () {
+    if (rain.classList.contains("rain_active")) {
+        rain.classList.remove("rain_active");
+    }
+})
