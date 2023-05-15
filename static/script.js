635ddd51315aacc97fd3159e4cad1faca97b4fcb
Vlad Shubin <shubin.va@phystech.edu>
2023-05-16 00:13:26 +0300

    begin to do message

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const theme_btn = document.querySelector(".header__theme-button");
+const themeBtn = document.querySelector(".header__theme-button");
 const contact_me = document.getElementById("contact_me");
 
 function ChangeTheme() {
@@ -31,14 +31,14 @@ function ChangeTheme() {
     elem.classList.toggle('footer__text_light');
 }
 
-theme_btn.addEventListener("click", ChangeTheme);
+themeBtn.addEventListener("click", ChangeTheme);
 
 
 
-const rain_btn = document.querySelector(".footer__rain-button");
+const rainBtn = document.querySelector(".footer__rain-button");
 let rain = document.querySelector(".rain");
 
-rain_btn.addEventListener('click', function (evt) {
+rainBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     if (window.innerWidth >= 800) {
         rain.classList.add("rain_active");
@@ -61,43 +61,66 @@ rain.addEventListener('click', function () {
 const popup = document.querySelector(".popup");
 popup.addEventListener('click', function(evt) {
     if (evt.target === popup) {
-        ClosePopup(popup);
+        closePopup(popup);
     }
     evt.stopPropagation();
 });
 
-const popup_img = document.querySelector(".popup__image");
+const popupImg = document.querySelector(".popup__image");
 const images = document.querySelectorAll(".popup-data__image");
-const popup_left_link = document.querySelector(".popup__link_left");
-const popup_right_link = document.querySelector(".popup__link_right");
+const popupLeftLink = document.querySelector(".popup__link_left");
+const popupRightLink = document.querySelector(".popup__link_right");
 
-images.forEach(function(image_element) {
-    const link = image_element.src;
-    image_element.addEventListener('click', function() {
-        popup_img.src = link;
-        check_links(image_element);
+images.forEach(function(imageElement) {
+    const link = imageElement.src;
+    imageElement.addEventListener('click', function() {
+        popupImg.src = link;
+        checkLinks(imageElement);
         popup.classList.add('popup_active');
-        image_element.classList.add("current");
+        imageElement.classList.add("current");
     });
 })
 
-function check_links(image_element) {
-    if (!image_element.nextElementSibling) {
-        popup_right_link.setAttribute('style', 'display: none');
+function checkLinks(imageElement) {
+    if (!imageElement.nextElementSibling) {
+        popupRightLink.setAttribute('style', 'display: none');
     } else {
-        popup_right_link.setAttribute('style', 'display: block');
+        popupRightLink.setAttribute('style', 'display: block');
     }
-    if (!image_element.previousElementSibling) {
-        popup_left_link.setAttribute('style', 'display: none');
+    if (!imageElement.previousElementSibling) {
+        popupLeftLink.setAttribute('style', 'display: none');
     } else {
-        popup_left_link.setAttribute('style', 'display: block');
+        popupLeftLink.setAttribute('style', 'display: block');
     }
 }
 
-function ClosePopup(popup) {
+function closePopup(popup) {
     popup.classList.remove('popup_active');
-    const current_image = document.querySelector('.current');
-    if (current_image) {
-        current_image.classList.remove("current");
+    const currentImage = document.querySelector('.current');
+    if (currentImage) {
+        currentImage.classList.remove("current");
     }
+}
+
+const messagePopupButton = document.querySelector(".message-popup__close-button");
+
+sendMessage()
+messagePopupClose()
+function helloPopupOpen() {
+    const popupMessage = document.querySelector(".message-popup");
+    popupMessage.classList.add("message-popup_active");
+}
+
+function sendMessage() {
+    if (!localStorage.getItem("send-flag")) {
+        setTimeout(helloPopupOpen, 3);
+    }
+}
+function messagePopupClose() {
+    messagePopupButton.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        localStorage.setItem("send-flag", "true");
+        const popupHello = document.querySelector(".message-popup_active");
+        popupHello.classList.remove("message-popup_active");
+    })
 }
